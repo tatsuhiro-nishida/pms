fded29c6006270648b00ad53eb78c8d88b1f89c5
tatsuhiro nishida <t_nishida@ucm.co.jp>
2020-12-07 12:59:18 +0900

    fix

--- src/entities/project.entity.ts
+++ src/entities/project.entity.ts
@@ -1,9 +1,25 @@
+import { ExecSyncOptionsWithBufferEncoding } from 'child_process';
 import { Entity, ObjectID, ObjectIdColumn, Column } from 'typeorm';
 
 @Entity('project')
 export class Project {
   @ObjectIdColumn() id: ObjectID;
   @Column() name: string;
+  @Column() isPick:boolean;
+  @Column() manager:string;
+  @Column() leader:string;
+  @Column() contract:string;
+  @Column() money:string;
+  @Column() fire:number;
+  @Column() status:number;
+  @Column() office:string[];
+  @Column() work:string;
+  @Column() process:string[];
+  @Column() member:number;
+  @Column() start:string;
+  @Column() end:string;
+  @Column() term:string;
+  @Column() memo:string;
 
   constructor(project?: Partial<Project>) {
     Object.assign(this, project);
